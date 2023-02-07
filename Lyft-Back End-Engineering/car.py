7259cfee581d30f272b56cee21d3887a8557b7f6
AchBenlakhdar <104871915+AchBenlakhdar@users.noreply.github.com>
2023-02-07 18:29:59 +0100

    Update car.py

--- Lyft-Back End-Engineering/car.py
+++ Lyft-Back End-Engineering/car.py
@@ -2,9 +2,10 @@ from abc import ABC, abstractmethod
 
 
 class Car(ABC):
-    def __init__(self, engine, battery):
+    def __init__(self, engine, battery, tires):
         self.engine=engine
         self.battery=battery
+        self.tires=tires
 
     def needs_service(self):
         self.engine.needs_service() or self.battery.needs_service()
